7d9cb178de6961a4089099ca0c7d8a850612b699
coderplug <dovydas.jurenkovas@mif.stud.vu.lt>
2018-11-22 14:23:21 +0200

    Add pizza info listing for pizza2go index page

--- pizza2go/views.py
+++ pizza2go/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Pizza
 
-
-# Create your views here.
-class IndexView(generic.TemplateView):
+class IndexView(generic.ListView):
     template_name = 'pizza2go/index.html'
+    context_object_name = 'ordered_pizza_list'
+
+    def get_queryset(self):
+        """Return all pizzas alphabetically."""
+        return Pizza.objects.order_by('name')
