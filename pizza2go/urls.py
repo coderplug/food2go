10b2631dc62e6468748799297a155060cd32c555
coderplug <dovydas.jurenkovas@mif.stud.vu.lt>
2018-12-03 20:39:18 +0200

    Fix base pages, add order template

--- pizza2go/urls.py
+++ pizza2go/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'pizza2go'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='pizza2go-index'),
     path('add_to_cart/', views.add_to_cart, name='pizza2go-add_to_cart'),
-    path('shopping_cart/', views.ShoppingCartView.as_view(), name='pizza2go-shopping_cart')
+    path('shopping_cart/', views.ShoppingCartView.as_view(), name='pizza2go-shopping_cart'),
+    path('orders/', TemplateView.as_view(template_name='pizza2go/orders.html'), name='pizza2go-orders')
 ]
